9030fd35ade1a3efedccb93d6e581126f12bc0f6
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2024-02-26 18:24:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,28 +13,28 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "360"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c27f34d40b4664d1a4db7.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/c27f34d40b4664d1a4db7.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/f8995139e4f503955150b.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/f8995139e4f503955150b.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PT_XV")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/UUU10K")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/darMahd313")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/darMahd313")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5627420357").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5695872306").split()))
 
 
 CHANNEL_SUDO = getenv(
-    "CHANNEL_SUDO", None
+    "CHANNEL_SUDO", "darMahd313"
 )
 
 YAFA_NAME = getenv(
-    "YAFA_NAME", None
+    "YAFA_NAME", "دار الاحــرار 313 💚"
 )
 
 YAFA_CHANNEL = getenv(
-   " YAFA_CHANNEL", None
+   " YAFA_CHANNEL", "https://t.me/darMahd313"
 )
 
 
-FAILED = "https://telegra.ph/file/c27f34d40b4664d1a4db7.jpg"
+FAILED = "https://telegra.ph/file/f8995139e4f503955150b.jpg"
